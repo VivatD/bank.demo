fd253338b25f3594683557c1922fbafd9c65037d
Nicolae Ghermacovschi <ghermacovschi.nicolae@gmail.com>
2023-06-10 13:16:02 +0300

    clientul in proces, test de incarcare pe git in bachk secundar1

--- src/main/java/com/bank/example/bank/demo/model/client/ClientChange.java
+++ src/main/java/com/bank/example/bank/demo/model/client/ClientChange.java
@@ -1,4 +1,57 @@
 package com.bank.example.bank.demo.model.client;
 
+import com.bank.example.bank.demo.model.currency.Currency;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "client_change")
 public class ClientChange extends Client{
+    @Column(name = "type_currensy")
+    private Currency typeCurrensy;
+    @Column(name = "amount_currency")
+    private long amountCurrency;
+
+    public ClientChange(){
+
+    }
+
+    public ClientChange(String firstName, String lastName, String idnp, String phoneNumber, Currency typeCurrensy,
+                        long amountCurrency) {
+        super(firstName, lastName, idnp, phoneNumber);
+        this.typeCurrensy = typeCurrensy;
+        this.amountCurrency = amountCurrency;
+    }
+
+    public ClientChange(long idClient, String firstName, String lastName, String idnp, String phoneNumber,
+                        Currency typeCurrensy, long amountCurrency) {
+        super(idClient, firstName, lastName, idnp, phoneNumber);
+        this.typeCurrensy = typeCurrensy;
+        this.amountCurrency = amountCurrency;
+    }
+
+    public Currency getTypeCurrensy() {
+        return typeCurrensy;
+    }
+
+    public void setTypeCurrensy(Currency typeCurrensy) {
+        this.typeCurrensy = typeCurrensy;
+    }
+
+    public long getAmountCurrency() {
+        return amountCurrency;
+    }
+
+    public void setAmountCurrency(long amountCurrency) {
+        this.amountCurrency = amountCurrency;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientChange{" +
+                "typeCurrensy=" + typeCurrensy +
+                ", amountCurrency=" + amountCurrency +
+                '}';
+    }
 }
--- src/main/java/com/bank/example/bank/demo/model/client/ClientReceiving.java
+++ src/main/java/com/bank/example/bank/demo/model/client/ClientReceiving.java
@@ -1,9 +1,74 @@
 package com.bank.example.bank.demo.model.client;
 
 import com.bank.example.bank.demo.model.currency.Currency;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "client_receiving")
 public class ClientReceiving extends Client{
+    @Column(name = "receving_destiation")
     private String recevingDestiation;
+    @Column(name = "type_currensy")
     private Currency typeCurrensy;
+    @Column(name = "amount_currency")
     private long amountCurrency;
+
+    public ClientReceiving(){
+
+    }
+
+    public ClientReceiving(String recevingDestiation, Currency typeCurrensy, long amountCurrency) {
+        this.recevingDestiation = recevingDestiation;
+        this.typeCurrensy = typeCurrensy;
+        this.amountCurrency = amountCurrency;
+    }
+
+    public ClientReceiving(String firstName, String lastName, String idnp, String phoneNumber, String recevingDestiation, Currency typeCurrensy, long amountCurrency) {
+        super(firstName, lastName, idnp, phoneNumber);
+        this.recevingDestiation = recevingDestiation;
+        this.typeCurrensy = typeCurrensy;
+        this.amountCurrency = amountCurrency;
+    }
+
+    public ClientReceiving(long idClient, String firstName, String lastName, String idnp, String phoneNumber, String recevingDestiation, Currency typeCurrensy, long amountCurrency) {
+        super(idClient, firstName, lastName, idnp, phoneNumber);
+        this.recevingDestiation = recevingDestiation;
+        this.typeCurrensy = typeCurrensy;
+        this.amountCurrency = amountCurrency;
+    }
+
+    public String getRecevingDestiation() {
+        return recevingDestiation;
+    }
+
+    public void setRecevingDestiation(String recevingDestiation) {
+        this.recevingDestiation = recevingDestiation;
+    }
+
+    public Currency getTypeCurrensy() {
+        return typeCurrensy;
+    }
+
+    public void setTypeCurrensy(Currency typeCurrensy) {
+        this.typeCurrensy = typeCurrensy;
+    }
+
+    public long getAmountCurrency() {
+        return amountCurrency;
+    }
+
+    public void setAmountCurrency(long amountCurrency) {
+        this.amountCurrency = amountCurrency;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientReceiving{" +
+                "recevingDestiation='" + recevingDestiation + '\'' +
+                ", typeCurrensy=" + typeCurrensy +
+                ", amountCurrency=" + amountCurrency +
+                '}';
+    }
 }
