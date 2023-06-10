9d84977392d57f4ad312620fe15527e49b32d02d
dumitruvivat <Atypic178>
2023-06-10 13:30:54 +0300

    add currency

---
+++ src/main/java/com/bank/example/bank/demo/model/currency/Balance.java
@@ -0,0 +1,10 @@
+package com.bank.example.bank.demo.model.currency;
+
+import java.math.BigDecimal;
+
+public class Balance {
+    private long id;
+    private Currency currency;
+    private BigDecimal amount;
+
+}
