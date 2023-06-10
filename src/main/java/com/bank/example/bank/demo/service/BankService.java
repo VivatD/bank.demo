8507e975c929418b3627b12fee23ec3e6b3b0534
grigore1986 <123980048+grigore1986@users.noreply.github.com>
2023-06-10 19:16:28 +0300

    Update BankService.java

--- src/main/java/com/bank/example/bank/demo/service/BankService.java
+++ src/main/java/com/bank/example/bank/demo/service/BankService.java
@@ -1,27 +1,34 @@
 package com.bank.example.bank.demo.service;
 
+import com.bank.example.bank.demo.model.bank.Bank;
+
 public class BankService  {
+    private  Bank bank;
+
+    private  BankService(Bank bank){
+        this.bank = bank;
+    }
     // Metode pentru a actualiza balanța băncii
     public  void addFunds(long amuons, String currency){
         if(currency.equals("MDL")){
-             balanceMDL = amuons;
+             bank.setBalanceMDL(bank.getBalanceMDL()+ amuons);
         } else if (currency.equals("USD")) {
-            balanceUSD += amuons;
+            bank.setBalanceUSD(bank.getBalanceUSD()+ amuons);
         } else if (currency.equals("EURO")) {
-            balanceEUR += amuons;
+            bank.setBalanceEUR(bank.getBalanceEUR()+ amuons);
         } else if (currency.equals("RON")) {
-            balanceRON += amuons;
+            bank.setBalanceRON(bank.getBalanceRON()+ amuons);
         }
     }
     public void withdrawFunds(long amouns, String currency){
         if(currency.equals("MDL")){
-            balanceMDL -= amouns;
+            bank.setBalanceMDL(bank.getBalanceMDL() - amouns);
         } else if (currency.equals("USD")) {
-            balanceUSD -= amouns;
+            bank.setBalanceUSD(bank.getBalanceUSD() - amouns);
         } else if (currency.equals("EURO")) {
-            balanceEUR -= amouns;
+            bank.setBalanceEUR(bank.getBalanceEUR() - amouns);
         } else if (currency.equals("RON")) {
-            balanceRON -= amouns;
+            bank.setBalanceRON(bank.getBalanceRON() - amouns);
         }
 
     }
